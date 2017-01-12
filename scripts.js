fcc7ae3fd8311926c9db6cb20d44ed9a6e2c9da0
Jesse Hill <jsnahill@gmail.com>
2017-01-11 18:50:47 -0600

    added css file for stylesheet.  added buttons to todo table.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
 $(function() {
-	
+	let todos = [
+		{
+			task: 'finish tutorials',
+			isCompleted: false,
+		}, 
+
+		{
+			task: 'take a nap',
+			isCompleted: true,
+		}
+	]
+
+	const app = {
+		showTodos: function() {
+			let todosListEl = $('#todos-list');
+
+			todosListEl.html('');
+
+			todos.forEach(function(todo) {
+				todosListEl.append('\
+						<tr>\
+							<td>' + todo.task + '</td>\
+							<td>\
+								<button>Edit</button>\
+								<button>Delete</button>\
+							</td>\
+						</tr>\
+					')
+			});
+		}
+	};
+
+	app.showTodos();
 });
